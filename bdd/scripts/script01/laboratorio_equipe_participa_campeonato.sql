1bf8dc0d87fe789fe36789bd0208ab74704c729b
BarbosaRodrigues <taisbarbosarodrigues2004@gmail.com>
2023-09-28 15:33:39 -0300

    scriptMySQL

---
+++ bdd/scripts/script01/laboratorio_equipe_participa_campeonato.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: laboratorio
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `equipe_participa_campeonato`
+--
+
+DROP TABLE IF EXISTS `equipe_participa_campeonato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipe_participa_campeonato` (
+  `equipe_id` int NOT NULL,
+  `nome_campeonato` varchar(100) NOT NULL,
+  PRIMARY KEY (`equipe_id`,`nome_campeonato`),
+  KEY `nome_campeonato` (`nome_campeonato`),
+  CONSTRAINT `equipe_participa_campeonato_ibfk_1` FOREIGN KEY (`equipe_id`) REFERENCES `equipe` (`id`),
+  CONSTRAINT `equipe_participa_campeonato_ibfk_2` FOREIGN KEY (`nome_campeonato`) REFERENCES `campeonato` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipe_participa_campeonato`
+--
+
+LOCK TABLES `equipe_participa_campeonato` WRITE;
+/*!40000 ALTER TABLE `equipe_participa_campeonato` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipe_participa_campeonato` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-28 15:29:09
