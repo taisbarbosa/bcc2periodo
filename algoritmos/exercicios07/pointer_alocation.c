e6e13e7d4b3da3c9c2e4931a51a372c58ca819bd
taisbarbosa <taisbarbosarodrigues2004@gmail.com>
2023-12-03 01:41:43 -0300

    gain

--- algoritmos/exercicios07/pointer_alocation.c
+++ algoritmos/exercicios07/pointer_alocation.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // q01. allocates an array of integers of size n, padded with zeros, on the heap, and returns your address
-
 int* create_vector(int n){
-    
-}
+    int *v = malloc(n * sizeof(int));
+    for(int i = 0; i<n; i++){
+        v[i] = 0;
+        printf("%p ", v[i]);
+    }
+    printf("\n %p  ", *v);
+    int *a = *v;
 
-int main(){}
+    free(v);
+    return a;
+}
+int main(){
+    printf("address: %p", create_vector(5));
+}
